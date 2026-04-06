c0f3a75aa810bac31c8e12d6a9a4fc333f3b7986
Sayantan-Maity-hub <maitysayantan116@gmail.com>
2026-04-06 22:30:51 +0530

    add SUPPORTED_BLOCKS_MAP for signal source block mapping

--- hil/scripts/detect_changed_blocks.py
+++ hil/scripts/detect_changed_blocks.py
@@ -3,3 +3,7 @@ import subprocess
 import sys
 from pathlib import Path
 
+SUPPORTED_BLOCKS_MAP ={
+    "gr-analog/lib/sig_source_impl.cc": "sig_source",
+    "gr-analog/lig/sig_source_impl.h": "sig_source",
+}
