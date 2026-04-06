37e1e7bbba8de7ab1918e4b8fee60e382100ca9b
Sayantan-Maity-hub <maitysayantan116@gmail.com>
2026-04-06 22:32:58 +0530

    add run_cmd function to execute shell commands and capture output

--- hil/scripts/detect_changed_blocks.py
+++ hil/scripts/detect_changed_blocks.py
@@ -7,3 +7,6 @@ SUPPORTED_BLOCKS_MAP ={
     "gr-analog/lib/sig_source_impl.cc": "sig_source",
     "gr-analog/lig/sig_source_impl.h": "sig_source",
 }
+def run_cmd(cmd: list[str]) -> str:
+    result = subprocess.run(cmd, capture_output=True, text=True, check=True)
+    return result.stdout.strip()
